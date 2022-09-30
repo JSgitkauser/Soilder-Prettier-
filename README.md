9b1fc9ce21da76488acf688d4ba74c702ea14fcd
JSgitkauser <114772370+JSgitkauser@users.noreply.github.com>
2022-10-01 00:44:28 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Oh_Soldier_Prettify_My_Folder
+This can rename all the files in a folder as per our needs and as per our filetype we can set properties and then use this programme as this can reduce human workload.
+
---
+++ main.py
@@ -0,0 +1,13 @@
+print("Hello Welcome to My Programme [Oh soldier Prettify my Folder] \n"
+	            "Please Mention the Following things \n"
+            "A Directory - from where we will find the folder\n"
+            "A File- in which the names should be there containing the folder that you don't want to rename\n"
+            "A format- from which we will use that format to change their name from 1 to the number of files of that format\n"
+            "Please Enter to Continue")
+space = input()
+
+
+soldier(r"Directory\Folder\Subfolder\Sub-sub-subfolfer",
+        r"Directory\Folder\SubFolder\SubFolder\Sub Folder\demo.txt", ".png" )
+
+print("Your Task Was Completed")
