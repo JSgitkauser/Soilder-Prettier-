e83ebfc7b9549080399b61d7aecf77767dc8ff55
Aryan Sonone <75265917+Coding-Aryan@users.noreply.github.com>
2022-10-01 01:01:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 print("Hello Welcome to My Programme [Oh soldier Prettify my Folder] \n"
 	            "Please Mention the Following things \n"
-            "A Directory - from where we will find the folder\n"
+            "A Directory - from where we will find the folder and locate it precisely\n"
             "A File- in which the names should be there containing the folder that you don't want to rename\n"
-            "A format- from which we will use that format to change their name from 1 to the number of files of that format\n"
+            "A format- from which we will use that format to change their name from 1 to the number of files of that format so that it looks mor pretty\n"
             "Please Enter to Continue")
 space = input()
 
@@ -10,4 +10,4 @@ space = input()
 soldier(r"Directory\Folder\Subfolder\Sub-sub-subfolfer",
         r"Directory\Folder\SubFolder\SubFolder\Sub Folder\demo.txt", ".png" )
 
-print("Your Task Was Completed")
+print("Your Task Was Completed Now you may try this for new folder with messed up files... ")
